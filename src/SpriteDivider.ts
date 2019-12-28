e605ab62e865d97c9089fcda0984e9a1bf076b52
worrysprite <worrysprite@gmail.com>
2019-12-28 14:51:20 +0800

    增加不同sprite格式的区分

--- src/SpriteDivider.ts
+++ src/SpriteDivider.ts
@@ -1,27 +1,50 @@
+#!/usr/bin/env node
 import sharp = require("sharp");
 import cli = require("cli");
 import fs = require("fs");
+import path = require("path");
 
-cli.setUsage("SpriteDivider.js sprite.json");
-cli.parse({});
+cli.setUsage("SpriteDivider.js [OPTIONS] <sprite.json>");
+cli.parse({
+	format: ['f', 'spritesheet format, support: TexturePacker, egret.', 'string', 'TexturePacker']
+});
 
 let jsonFile = cli.args[0];
-if (!jsonFile) {
-	cli.error("image file must be specified");
+if (!jsonFile || path.extname(jsonFile) != ".json") {
+	cli.error("spritesheet json must be specified!");
 	cli.exit(-1);
 }
 
+let format = cli.options.format;
+let basedir = path.dirname(jsonFile);
 fs.readFile(jsonFile, (err, data) => {
 	if (err) {
 		cli.error(err.message);
 		cli.exit(-1);
 	}
-	let frameData = JSON.parse(data.toString());
-	let outPath = frameData.meta.prefix;
+	switch (format) {
+		case 'TexturePacker':
+			readTexturePackerFormat(JSON.parse(data.toString()));
+			break;
+	
+		case 'egret':
+			readEgretFormat(JSON.parse(data.toString()));
+			break;
+	
+		default:
+			cli.error("unsupported spritesheet format!");
+			cli.exit(-1);
+			break;
+	}
+});
+
+
+function readTexturePackerFormat(frameData) {
+	let outPath = path.resolve(basedir, frameData.meta.prefix);
 	let images = frameData.meta.image.split(",");
 	let tmp = [];
 	for (let i = 0; i < images.length; i++) {
-		tmp.push(sharp(images[i]));
+		tmp.push(sharp(path.resolve(basedir, images[i])));
 	}
 	images = tmp;
 
@@ -41,6 +64,31 @@ fs.readFile(jsonFile, (err, data) => {
 			console.log(err.message);
 		});
 	}
-});
+}
+
+function readEgretFormat(frameData) {
+	let image = sharp(path.resolve(basedir, frameData.file));
+	if (!image) {
+		cli.error(`image ${frameData.file} not found!`);
+		cli.exit(-1);
+	}
+	
+	let outPath = path.resolve(basedir, path.basename(jsonFile, '.json'));
+	if (!fs.existsSync(outPath)) {
+		fs.mkdirSync(outPath);
+	}
+	
+	let frames = frameData.frames;
+	for (const key in frames) {
+		const frame = frames[key];
+		const fileName = key.replace(/_png$/, '.png').replace(/_jpg$/, '.jpg');
+		image.clone().extract({
+			left: frame.x, top: frame.y, width: frame.w, height: frame.h
+		}).toFile(path.resolve(outPath, fileName)).catch(err => {
+			console.log(err.message);
+		});
+	}
+}
+
 
 
